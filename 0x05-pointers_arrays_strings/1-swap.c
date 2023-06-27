32f2b686f6d749ae9d4c05cc5f48b3fa86b66e8a
cmosongo <charles.mosongo@strathmore.edu>
2023-06-27 15:15:57 -0700

    adj betty doc

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
  * swap_int -  a function that swaps the values of two integers.
- * @a - args int pointer
- * @b - args int pointer
+ * @a : args int pointer
+ * @b : args int pointer
  * Return: Nothing
  */
 void swap_int(int *a, int *b)
